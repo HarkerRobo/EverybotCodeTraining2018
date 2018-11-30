95ec09ccd9b39a726fb0e8d511bdb36dfe565da2
Aimee Wang <22aimeew@students.harker.org>
2018-11-29 17:28:54 -0800

    Create the intake subsystem

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,10 @@
+package frc.robot;
+
+public class RobotMap
+{
+    public static class CAN_IDs
+    {
+        public static final int INTAKE_LEFT = 1;
+        public static final int INTAKE_RIGHT = 4;
+    }
+}
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,7 +1,27 @@
 package frc.robot.subsystems;
 
+import frc.robot.RobotMap.CAN_IDs;
 import harkerrobolib.subsystems.HSIntake;
+import harkerrobolib.wrappers.HSTalon;
 
 public class Intake extends HSIntake {
-    
+
+    private static Intake intake;
+
+    private Intake()
+    {
+        super(new HSTalon(CAN_IDs.INTAKE_LEFT), new HSTalon(CAN_IDs.INTAKE_RIGHT));
+    }
+
+    public static Intake getInstance()
+    {
+        if(intake == null);
+            intake = new Intake();
+        return intake;
+    }
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
 }
